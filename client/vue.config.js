ace8a78287dd0b97f57efe4938aea23a958c6605
Ajesh <ajesh.avirah@cse.christuniversity.in>
2018-11-27 14:15:23 +0530

    heroku ignore file - .slugignore

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 // 73: export the proxy - devServer
 module.exports = {
     // 73.1: Creating Dist or Publishing
-        outputDir: path.resolve(__dirname, '../server/public'),
+        outputDir: path.resolve(__dirname, '../crud-mevn-post/server/public'),
     // 73.2: Creating proxy for devServer
     devServer: {
         proxy: {
